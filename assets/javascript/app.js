16da7c58662fe70bcf2b1be5fbae3f07fdea8e78
Michael Bizzle <mbizzle@studiomoviegrill.com>
2018-02-26 17:12:02 -0600

    updated files.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,50 +7,58 @@ $(document).ready(function () {
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 var unansweredQuestions = 0;
-var timeRemaining = 15;
+var timeRemaining = 16;
 var intervalID;
 var indexQandA = 0; 
 var answered = false; 
 var correct;
 var triviaGame = [{
-        question: "What is the name for a dog created by crossing a Labrador Retriever and a Poodle?",
-        answer: ["labbypoodle", "labradoodle", "poodlelab", "poodledoodle"],
+        question: " In the episode 'The Death of Eric Cartman', why do the boys ignore Cartman?",
+        answer: ["He called Kyle a Jew", "He ate the skins off their KFC Chicken", "He calls Wendy a skinny bitch", "Cartman bets the guys he can sell more records with a Christian Rock Band"],
         correct: "1", 
+        image: ("assets/images/kfc.gif")
 
     }, {
-        question: "The Chihuahua is a breed of dog believed to originate from what country?",
-        answer: ["mexico", "america", "peru", "ecuador"],
+        question: "What is the name of the school counselor for South Park Elementary?",
+        answer: ["Mr. Mackey", "Mr. Garrison", "Mrs. Garrison", "PC Principle"],
         correct: "0",
+        image: ("assets/images/mackey.gif")
 
     }, {
-        question: "What is the most popular breed of dog in the United States?",
-        answer: ["german sherpard", "pug", "labrador retriever", "irish settler"],
+        question: "What does Towelie like to do?",
+        answer: ["Play video games", "No idea", "Get High", "Help out the boys"],
         correct: "2",
+        image: ("assets/images/towlie.gif")
 
     }, {
-        question: "The Alaskan Malamute is a type of what?",
-        answer: ["moose", "wolf", "cat", "dog"],
+        question: "What's the name of Cartman's Boy Band",
+        answer: ["Moop", "Faith+1", "Red Hot Catholic Love", "Fingerbang"],
         correct: "3",
+        image: ("assets/images/fingerbang.gif")
 
     }, {
-        question: "A puggle is a cross between which two dog breeds?",
-        answer: ["pug and beagle", "beagle and poodle", "pug and poodle", "beagle and terrier"],
+        question: "What is the term that Cartman uses for ginger people that don't have pale skin or freckles?",
+        answer: ["Daywalkers", "Gingers", "Half Ginger", "Gingervitis"],
         correct: "0",
+        image: ("assets/images/daywalker.gif")
 
     }, {
-        question: "How many chambers are there in a dog's heart?",
-        answer: ["1", "3", "6", "4"],
+        question: "What does Kyle pledge to break after Cartman gives Kyle AIDs?",
+        answer: ["Okama GameSphere", "Chinpokomon", "PSP", "Xbox 360"],
         correct: "3",
+        image: ("assets/images/aids.gif")
 
     }, {
-        question: "In the United States which breed of dog is commonly known as a firehouse dog?",
-        answer: ["german sherpard", "dalmatian", "blue heeler", "boxer"],
+        question: "What religion is Stan drawn to when is said to be the reincarnation of the religion's founder?",
+        answer: ["Judaism", "Scientology", "Catholicism", "Mormon"],
         correct: "1",
+        image: ("assets/images/stan.gif")
 
     }, {
-        question: "The dingo is a free ranging dog found mainly in which country?",
-        answer: ["ireland", "new zealand", "australia", "japan"],
+        question: "What is the name of the club Chef joins in the episode 'The Return of Chef?'",
+        answer: ["Unified Atheist League", "Allied Atheist Alliance", "Super Adventure Club", "Hare Club for Men"],
         correct: "2",
+        image: ("assets/images/adventure.gif")
 
     }]; 
 
@@ -59,6 +67,8 @@ var triviaGame = [{
 // This function starts the game
 function startGame() {
         $(".start-button").remove();
+        $("h1").remove();   
+        $(".jumbotron").css("background-image", "none");
         correctAnswers = 0;
         incorrectAnswers = 0;
         unansweredQuestions = 0;
@@ -70,78 +80,95 @@ function loadQandA() {
     // This will set the variable answered to false, timeRemaining to 15
     // and we are setting variable intervalID to setInterval to a timer and to countdown 1000ms 
     answered = false;   
-    timeRemaining = 15;
+    timeRemaining = 16;
     intervalID = setInterval (timer, 1000); 
 
     // we create an if/else statement 
+    // If answered is false, then we wiull run function timer()
     if (answered === false) { 
         timer();    
     } 
-    // This line is indicating that if answered is false, then the correct variable will load the 
-    //correct variable from the triviaGame object variable into the indexQandA array. 
-    // We will then create a question variable to do the same thing for the question. 
-    // We will post the variable question 
-    correct = triviaGame[indexQandA].correct;   
+    // We create a correct variable to store the correct answer to the triviaGame object variables above. 
+    // We also place the triviaGame Object variable into the indexQandA variable so we can cycle through the various items in the object variable.
+    correct = triviaGame[indexQandA].correct; 
+    // We create a question variable to load the triviaGame Questions from the variable object. 
     var question = triviaGame[indexQandA].question; 
+    // We load the question variable into the question class 
     $(".question").html(question);  
+    // We need to loop through the answer options in each question so we create a for loop 
     for (var i = 0; i < 4; i++) {
+        // we create an answer variable that will populate the answer options available.  
         var answer = triviaGame[indexQandA].answer[i];
+        // We then append these answer options to the answers class.  We create a class and a unique ID for each answer. 
         $('.answers').append('<h4 class= answersAll id=' + i + '>' + answer + '</h4>');
     }
+
+    // This click function is where we call out the right or wrong answers in the game. 
     $("h4").click(function () {
+        //We are stating when the h4 tag is click, we look up the unique ID placed on the h4 tag.
         var id = $(this).attr('id');
         // alert(id);
+        // If else statement is describing if the id above matches the correct variable in the triviaGameObject,
+        //then state the answer and run either the correctAnswer or incorrectAnswer functions.  
         if (id === correct) {
             answered = true; // stops the timer
             // alert("correct answer");
-            $('.question').text("Yo, the answer is fo shizzle: " + triviaGame[indexQandA].answer[correct]);
+            $('.question').text(triviaGame[indexQandA].answer[correct] + ". You member!");
             correctAnswer();
         } else {
             answered = true; //stops the timer
             // alert("incorrect answer");
-            $('.question').text("YOU CHOSE: " + triviaGame[indexQandA].answer[id] + ".....HOWEVER THE ANSWER IS: " + triviaGame[indexQandA].answer[correct]);
+            $('.question').text(triviaGame[indexQandA].answer[id] + "?.....guess you didn't member. The correct answer is: " + triviaGame[indexQandA].answer[correct] + ".");
             incorrectAnswer();
         }
     });
 }
-
+//This function controls the timer in the rounds 
 function timer() {
+    // This if else statement is creating the conditions necessary to clear out the intervals for each question. 
     if (timeRemaining === 0) {
         answered = true;
         clearInterval(intervalID);
-        $('.question').text("THE CORRECT ANSWER IS: " + triviaGame[indexQandA].answer[correct]);
+        $('.question').text("Member: " + triviaGame[indexQandA].answer[correct] + "!");
         unAnswered();
     } else if (answered === true) {
         clearInterval(intervalID);
     } else {
         timeRemaining--;
-        $('.timeRemaining').text('YOU HAVE ' + timeRemaining + ' SECONDS TO CHOOSE');
+        $('.timeRemaining').text('You have ' + timeRemaining + ' seconds to member.');
     }
 }
 
+// This function creates the conditions to increment the correct answers varaible while also reseting the round. 
 function correctAnswer() {
     correctAnswers++;
-    $('.timeRemaining').text("YOU HAVE ANSWERED CORRECTLY!")
+    $('.timeRemaining').text("Oh, you member!")
     resetRound();
 }
 
+// This function creates the conditions to increment the incorrect answers varaible while also reseting the round.
 function incorrectAnswer() {
     incorrectAnswers++;
-    $('.timeRemaining').text("YOU HAVE ANSWERED INCORRECTLY!")
+    $('.timeRemaining').text("Oh no, you don't member!")
     resetRound();
 
 }
 
+// This function creates the conditions to increment the unanswered varaible while also reseting the round.
 function unAnswered() {
     unansweredQuestions++;
-    $('.timeRemaining').text("YOU FAILED TO CHOOSE AN ANSWER")
+    $('.timeRemaining').text("Oh no! You didn't member fast enough!")
     resetRound();
 }
 
+// This function creates the conditions to reset the rounds in between each question asked. 
 function resetRound() {
+    // This allowsus to remove the previous answers 
     $('.answersAll').remove();
-    $('.answers').append('<img class=answerImage width="150" height="150" src="' + triviaGame[indexQandA].image + ' ">'); // adds answer image
-    indexQandA++; // increments index which will load next question when loadQandA() is called again
+    //This gives us an image associated with the previous question 
+    $('.answers').append('<img class=answerImage img-responsive src="' + triviaGame[indexQandA].image + ' ">'); 
+    // increments index which will load next question when loadQandA() is called again
+    indexQandA++; 
     if (indexQandA < triviaGame.length) {
         setTimeout(function () {
             loadQandA();
@@ -152,9 +179,10 @@ function resetRound() {
             $('.question').remove();
             $('.timeRemaining').remove();
             $('.answerImage').remove();
-            $('.answers').append('<h4 class= answersAll end>CORRECT ANSWERS: ' + correctAnswers + '</h4>');
-            $('.answers').append('<h4 class= answersAll end>INCORRECT ANSWERS: ' + incorrectAnswers + '</h4>');
+            $('.answers').append('<h4 class= answersAll end>CORRECT MEMBERS: ' + correctAnswers + '</h4>');
+            $('.answers').append('<h4 class= answersAll end>INCORRECT MEMBERS: ' + incorrectAnswers + '</h4>');
             $('.answers').append('<h4 class= answersAll end>UNANSWERED QUESTIONS: ' + unansweredQuestions + '</h4>');
+             $('.answers').append('<img class=answerImage img-responsive width="400" height="300" src="assets/images/member.jpg">');
             setTimeout(function () {
                 location.reload();
             }, 7000);
